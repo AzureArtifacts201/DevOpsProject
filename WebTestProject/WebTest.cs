a3686a4794fcfe2eedd217e22b98a7aa1ba459b5
aprashar-epam <Achal_Parashar@epam.com>
2022-09-24 21:39:47 +0530

    added test method

--- WebTestProject/WebTest.cs
+++ WebTestProject/WebTest.cs
@@ -1,11 +1,15 @@
 namespace WebTestProject
 {
-    public class UnitTest1
+    public class WebTest
     {
         [Fact]
-        public void Test1()
+        public void WebTestMethod()
         {
-
+            int i = 1;
+            bool result = false;
+            if (i == 1)
+                result = true;
+            Assert.True(result, "value should be equal to  1");
         }
     }
 }
